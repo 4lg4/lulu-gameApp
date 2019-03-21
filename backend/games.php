762fc243682e743afdb47a592c3abae34fed713f
Alga <akgleal@gmail.com>
2019-03-22 10:25:42 +1100

    games route

--- backend/games.php
+++ backend/games.php
@@ -1,16 +1,15 @@
 <?php
 
 function getGames(){
-	$con = conectaDB();
+	$con = dbConnect();
 	$result = mysqli_query($con,"SELECT titulos.nome,fabricantes.nome,preco,classificacao,titulos.cod FROM titulos,fabricantes WHERE titulos.fabricante  =  fabricantes.cod ORDER BY titulos.nome"); 		
-	mostraTabela(1,$result,'Jogo');
+	dbGetContent(1,$result,'Jogo');
 	$con->close();
 }
 
 // Route users
 if(@$_REQUEST['action'] == "games") {
 	echo "get games";
-	// getGames();
-
+	getGames();
 	exit;
 }
