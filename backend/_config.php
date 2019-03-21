4ddcd0ec2ef663f37b41c37ba5cf783a723ba383
Alga <akgleal@gmail.com>
2019-03-22 10:26:20 +1100

    ignoring config file

--- backend/_config.php
+++ backend/_config.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DB_HOST' , 'localhost');
+define('DB_HOST' , '192.168.100.100');
 define('DB_USER' , 'root');
-define('DB_PASSWORD' , '');
-define('DB_DB' , 'games');
+define('DB_PASSWORD' , 'magic');
+define('DB_DB' , 'lulu-games');
