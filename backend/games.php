3e8b4abdb8993f9455df2d8df497937170b26db9
Alga <akgleal@gmail.com>
2019-03-22 11:22:31 +1100

    games service finished

--- backend/games.php
+++ backend/games.php
@@ -2,14 +2,55 @@
 
 function getGames(){
 	$con = dbConnect();
-	$result = mysqli_query($con,"SELECT titulos.nome,fabricantes.nome,preco,classificacao,titulos.cod FROM titulos,fabricantes WHERE titulos.fabricante  =  fabricantes.cod ORDER BY titulos.nome"); 		
-	dbGetContent(1,$result,'Jogo');
+	$r = dbGetContent(
+		mysqli_query($con, "SELECT titulos.nome,fabricantes.nome,preco,classificacao,titulos.cod FROM titulos,fabricantes WHERE titulos.fabricante = fabricantes.cod ORDER BY titulos.nome")
+	);
+	$con->close();
+	return $r;
+}
+
+function deleteGames($id){
+	$con = dbConnect();
+	$res = mysqli_query($con, "DELETE FROM titulos WHERE titulos.cod  =  ".$id);
 	$con->close();
 }
 
-// Route users
+function createGames($game){
+	$con = dbConnect();
+
+	$con = conectaDB();
+	$jogo = dbScapeString($game['jogo']);
+	$fabricante = dbScapeString($game['fab']);
+	$preco = floatval($game['preco']);
+	$classificacao = intval($game['class']);
+	
+	mysqli_query($con,"INSERT INTO titulos (nome,fabricante,preco,classificacao) VALUES('$jogo','$fabricante','$preco','$classificacao');");
+	$con->close();
+}
+
+// Routes users
+// GET games
 if(@$_REQUEST['action'] == "games") {
-	echo "get games";
-	getGames();
+	httpSuccess(getGames());
+	exit;
+}
+
+// DELETE games
+if(@$_REQUEST['action'] == "gamesDelete") {
+	deleteGames($_REQUEST['id']);
+	httpSuccess(getGames());
+	exit;
+}
+
+// CREATE games
+if(@$_REQUEST['action'] == "gamesCreate") {
+	createGames([
+		'jogo' => $_REQUEST['jogo'],
+		'fab' => $_REQUEST['fab'],
+		'preco' => $_REQUEST['preco'],
+		'class' => $_REQUEST['class'],
+	]);
+	
+	httpSuccess(getGames());
 	exit;
 }
