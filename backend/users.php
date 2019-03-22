3ccba6212a55fae5dadf1b0b9daa124e01e450d8
Alga <akgleal@gmail.com>
2019-03-22 11:22:41 +1100

    users service finished

--- backend/users.php
+++ backend/users.php
@@ -1,16 +1,61 @@
 <?php
 
 function getUsers() {
-	$con = conectaDB();
-	$result = mysqli_query($con,"SELECT usuarios.nome,nick,cidades.nome,email,idade,usuarios.cod FROM usuarios,cidades WHERE usuarios.cidade  =  cidades.cod ORDER BY usuarios.nome"); //eh retornada nesta consulta o campo de codigo do usuario (na ultima posicao) 		
-	mostraTabela(5,$result,'Usuario');    //este codigo eh usado como parametro na funcao javascript de delecao
-	$con->close();                        //o html da chamada desta funcao de delecao eh montado na funcao php mostraTabela
+	$con = dbConnect();
+	$r = dbGetContent(
+		mysqli_query($con,"SELECT usuarios.nome,nick,cidades.nome,email,idade,usuarios.cod FROM usuarios,cidades WHERE usuarios.cidade  =  cidades.cod ORDER BY usuarios.nome")
+	);
+	$con->close();
+	return $r;
 }
 
-// Route users
-if(@$_REQUEST['action'] == "users") {
-	echo "get users";
-	// getUsers();
+function deleteUsers($id) {
+	$con = dbConnect();
+	$res = mysqli_query($con,"DELETE FROM usuarios WHERE usuarios.cod  =  ".$id);
+	$con->close();
+}
 
+function createUsers($user) {
+	$con = dbConnect();
+
+	$nomeUsuario = dbScapeString($user['usuario']);
+	$nick = dbScapeString($user['nick']);
+	$email = dbScapeString($user['email']);
+	$idade = intval($user['idade']);
+	if($idade == "") {
+		$idade = "NULL";
+	}  //no input do form em index.html eh empregado min = "1" para limitar a idade minima a 1
+	$cidade = dbScapeString($user['cidade']);
+	
+	mysqli_query($con,"INSERT INTO usuarios (nome,nick,email,idade,cidade) VALUES('$nomeUsuario','$nick','$email','$idade','$cidade');");
+
+	$con->close();
+}
+
+// Routes users
+// GET users
+if(@$_REQUEST['action'] == "users") {
+	httpSuccess(getUsers());
+	exit;
+}
+
+// CREATE users
+if(@$_REQUEST['action'] == "usersCreate") {
+	createUsers([
+		'usuario'=> $_REQUEST['usuario'],
+		'email'=> $_REQUEST['email'],
+		'nick'=> $_REQUEST['nick'],
+		'idade'=> $_REQUEST['idade'],
+		'cidade'=> $_REQUEST['cidade'],
+	]);
+
+	httpSuccess(getUsers());
+	exit;
+}
+
+// DELETE users
+if(@$_REQUEST['action'] == "usersDel") {
+	deleteUsers($_REQUEST['id']);
+	httpSuccess(getUsers());
 	exit;
 }
